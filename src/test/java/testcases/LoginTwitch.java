c88c96f216fb1aa8107fb57b19ac61b68e8f2aac
klo <pynnyp1914@gmail.com>
2020-09-13 19:01:04 -0500

    Addint test.

---
+++ src/test/java/testcases/LoginTwitch.java
@@ -0,0 +1,28 @@
+package testcases;
+
+import main.elements.LoginTwitchPage;
+import main.steps.LoginPage;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
+public class LoginTwitch {
+
+    LoginTwitchPage url;
+    @Managed
+    WebDriver webDriver;
+    @Steps
+    LoginPage loginPage;
+
+
+
+    @Test
+    public void login(){
+        webDriver.get("https://www.google.com");
+        loginPage.searchWebTwitch();
+    }
+}
